ee186027d5e315981571cc3a1411cde15eeb3c6f
valterapolinario <valter_123@hotmail.com>
2020-12-11 11:59:24 -0300

    projeto criado e sincronizado com a IDE

---
+++ src/main/java/com/example/apiRestBetlab/ApiRestBetlabApplication.java
@@ -0,0 +1,13 @@
+package com.example.apiRestBetlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestBetlabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestBetlabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/apiRestBetlab/ApiRestBetlabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apiRestBetlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestBetlabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
