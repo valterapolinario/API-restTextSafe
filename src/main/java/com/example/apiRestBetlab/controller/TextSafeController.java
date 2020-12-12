4f6fc3f6f6c84d1adf44bf9adb7dbb08c527fb05
valterapolinario <valter_123@hotmail.com>
2020-12-11 21:30:15 -0300

    paginação implementada , ordenação em andamento

--- src/main/java/com/example/apiRestBetlab/controller/TextSafeController.java
+++ src/main/java/com/example/apiRestBetlab/controller/TextSafeController.java
@@ -4,6 +4,10 @@ package com.example.apiRestBetlab.controller;
 import com.example.apiRestBetlab.dto.TextSafeDTO;
 import com.example.apiRestBetlab.services.TextSafeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,9 +22,16 @@ public class TextSafeController {
 
 
     @GetMapping
-    public ResponseEntity<List<TextSafeDTO>>findAll(){
+    public ResponseEntity<Page<TextSafeDTO>>findAll(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "linesPerPage",defaultValue = "10") Integer linesPerPage,
+            @RequestParam(value = "orderBy",defaultValue = "id") String orderBy,
+            @RequestParam(value = "direction",defaultValue = "ASC") String direction,
+            Pageable pageable){
 
-        return ResponseEntity.ok().body(service.findAll());
+        PageRequest pageRequest = PageRequest.of(page,linesPerPage, Sort.Direction.valueOf(direction),orderBy);
+
+        return ResponseEntity.ok().body(service.findAll(pageable));
     }
     @PostMapping
     public ResponseEntity<TextSafeDTO> insert (@Valid @RequestBody TextSafeDTO objectTextSafeDto){
--- src/main/java/com/example/apiRestBetlab/services/TextSafeService.java
+++ src/main/java/com/example/apiRestBetlab/services/TextSafeService.java
@@ -5,6 +5,9 @@ import com.example.apiRestBetlab.dto.TextSafeDTO;
 import com.example.apiRestBetlab.model.TextSafe;
 import com.example.apiRestBetlab.repository.TextSafeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,8 +22,8 @@ public class TextSafeService {
 
 
     @Transactional(readOnly = true)
-    public List<TextSafeDTO>findAll(){
-        return dao.findAll().stream().map(objectTextSafe -> new TextSafeDTO(objectTextSafe)).collect(Collectors.toList());
+    public Page<TextSafeDTO> findAll(Pageable pageable){
+        return dao.findAll(pageable).map(objectTextSafe -> new TextSafeDTO(objectTextSafe));
     }
 
     @Transactional
