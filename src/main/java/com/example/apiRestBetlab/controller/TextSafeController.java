bfc600af365934a56e3cbc434555c3ead8b2e20a
valterapolinario <valter_123@hotmail.com>
2020-12-14 09:58:42 -0300

    mudança na direção da paginação de registros

--- src/main/java/com/example/apiRestBetlab/controller/TextSafeController.java
+++ src/main/java/com/example/apiRestBetlab/controller/TextSafeController.java
@@ -7,15 +7,11 @@ import com.example.apiRestBetlab.model.TextSafe;
 import com.example.apiRestBetlab.services.TextSafeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.List;
 
 @RestController
 @RequestMapping(value = "/textsafes")
@@ -29,7 +25,7 @@ public class TextSafeController {
             @RequestParam(value = "page", defaultValue = "0") Integer page,
             @RequestParam(value = "linesPerPage",defaultValue = "10") Integer linesPerPage,
             @RequestParam(value = "orderBy",defaultValue = "id") String orderBy,
-            @RequestParam(value = "direction",defaultValue = "DESC") String direction){
+            @RequestParam(value = "direction",defaultValue = "ASC") String direction){
 
         Page<TextSafe> listOfPages = service.findAllPages(page,linesPerPage,orderBy,direction);
         Page<TextSafeDTO> listOfPagesDto = listOfPages.map((objectOfSavedTextPages -> SavedTextConverter.convertToDto(objectOfSavedTextPages)));
